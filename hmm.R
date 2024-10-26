be660affa7c7b1706b5dc51396c24bb50c2218d6
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2024-10-26 15:27:57 -0400

    Fuzzy matching

--- hmm.R
+++ hmm.R
@@ -9,7 +9,8 @@ litter %>%
   filter(year(Date) == 2020) %>% 
   group_by(Plot, Trap) %>% 
   summarise(ACRU = sum(`Leaf-ACRU`), FAGR = sum(`Leaf-FAGR`), 
-            LITU = sum(`Leaf-LITU`), Other = sum(`Leaf-Other`)) ->
+            LITU = sum(`Leaf-LITU`), Other = sum(`Leaf-Other`),
+            .groups = "drop") ->
   litter
 
 litter$Total = rowSums(litter[c("ACRU", "FAGR", "LITU", "Other")])
@@ -65,7 +66,7 @@ print(result)
 
 # Temporary - fake grid locations
 grid_locations <- expand_grid(Plot = c("C", "F", "S"), Trap = LETTERS[1:12])
-grid_locations$Grid <- sample(unique(trees$Grid), size = nrow(grid_locations))
+grid_locations$Grid <- sample(unique(na.omit(trees$Grid)), size = nrow(grid_locations))
 
 litter %>% 
   left_join(grid_locations, by = c("Plot", "Trap")) %>% 
@@ -76,6 +77,57 @@ litter %>%
 # Exact grid square matching
 litter_long %>% 
   left_join(trees, by = c("Plot", "Grid", "Species")) %>% 
-  replace_na(list(Leaf = 0))
+  replace_na(list(Leaf = 0)) ->
+  combined_exact
+
+library(ggplot2)
+p <- ggplot(combined_exact, aes(Litter, Leaf, color = Species)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE) +
+  facet_grid(.~Plot) + 
+  ggtitle("Exact grid square matching")
+print(p)
+ggsave("exact-matching.png", width = 8, height = 4)
 
 # Fuzzy match - any tree within a 3x3 centered around the trap is included
+
+# Helper function to compute the 3x3 grid around a litter trap
+# For example, for C2 we want {B1, B2, B3, C1, C2, C3, D1, D2, D3}
+makebox <- function(loc) {
+  if(!grepl("^[A-J][1-8]$", loc)) return(NULL) # make sure data is in expected form
+  
+  x <- which(LETTERS == substr(loc, 1, 1)) 
+  xseq <- seq(x - 1, x + 1)
+  xseq <- xseq[xseq > 0 & xseq < 11] 
+  y <- as.integer(substr(loc, 2, 2))
+  yseq <- seq(y - 1, y + 1)
+  yseq <- yseq[yseq > 0 & yseq < 9]
+  df <- expand.grid(LETTERS[xseq], yseq)
+  
+  return(paste0(df[,1], df[,2]))
+}
+
+# For each litter_long plot and grid, get the trees in surrounding box
+ll_split <- split(litter_long, paste(litter_long$Plot, litter_long$Grid))
+results <- list()
+for(i in seq_along(ll_split)) {
+  x <- ll_split[[i]]
+  # Find the nearby grid squares and trees within them
+  nearby <- makebox(x$Grid[1])
+  y <- trees[trees$Plot == x$Plot[1] & trees$Grid %in% nearby,]
+  # Add them up, join with trees, and store
+  y_total <- y %>% group_by(Species) %>% summarise(Leaf = sum(Leaf))
+  x %>% left_join(y_total, by = "Species") -> results[[i]]
+}
+
+results %>% 
+  bind_rows() %>% 
+  replace_na(list(Leaf = 0)) ->
+  combined_fuzzy
+
+p_fuzzy <- p %+% combined_fuzzy
+p_fuzzy <- p_fuzzy + ggtitle("Fuzzy (3x3) grid square matching")
+print(p_fuzzy)
+ggsave("fuzzy-matching.png", width = 8, height = 4)
+
+
